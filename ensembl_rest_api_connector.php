b1fa23b53b90df922c5c043dd754e718c8e78355
Samuel Lampa <samuel.lampa@gmail.com>
2013-05-31 14:05:32 +0200

    Bug fix

--- ensembl_rest_api_connector.php
+++ ensembl_rest_api_connector.php
@@ -3,9 +3,11 @@
 // Test the new Ensemble REST API with an example gene
 $ensemblID = "ENSG00000157764";
 $ensemblResultJson = file_get_contents("http://beta.rest.ensembl.org/xrefs/id/$ensemblID?content-type=application/json");
-$ensemblResult = json_decode($ensemblResultJson, true);A
+$ensemblResult = json_decode($ensemblResultJson, true);
 
 // Print out the structure of the resulting array structure
+echo "<pre>";
 print_r($ensemblResult);
+echo "</pre>";
 
-
+?>
